cb4c658594cf8e56ad830ed13ad65d1d8bb9af6e
Jakob Rath <git@jakobrath.eu>
2013-12-17 11:47:31 +0100

    go-gui 1.4.9
    Closes #86.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- go-gui.rb
+++ go-gui.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class GoGui < Formula
   homepage 'http://gogui.sourceforge.net'
-  url 'http://downloads.sourceforge.net/project/gogui/gogui/1.4.7/gogui-1.4.7.zip'
-  sha1 'f9237e50594eafcb3d18e0b5238e32e28af72f1b'
+  url 'http://downloads.sourceforge.net/project/gogui/gogui/1.4.9/gogui-1.4.9.zip'
+  sha1 'b4c402fa42ea054e251642e56a5da0be890c24e5'
 
   depends_on :ant
 

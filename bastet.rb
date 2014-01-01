6deaeebb1a7dfd4734cf8d2e29845213ade6a5e3
phySi0 <ha.alamin@gmail.com>
2014-01-01 12:03:28 +0000

    bastet 0.41
    A Tetris clone that tries to figure out what is the worst block to give you, then gives you that, as opposed to randomly assigning a block.
    
    Closes #90.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

---
+++ bastet.rb
@@ -0,0 +1,30 @@
+require "formula"
+
+class Bastet < Formula
+  homepage "http://fph.altervista.org/prog/bastet.html"
+  url "http://fph.altervista.org/prog/files/bastet-0.41.tgz"
+  sha1 "644a0f76adedef84946159520c1639ff0c6c47ec"
+
+  patch :p1 do
+    url "http://fph.altervista.org/prog/files/bastet-0.41-osx-patch.diff"
+    sha1 "bf38253d07889025c05440a77b4a498dfd952a2c"
+  end
+
+  def install
+    inreplace "Makefile" do |s|
+      s.change_make_var! "BIN_PREFIX", "#{bin}/"
+      s.change_make_var! "DATA_PREFIX", "#{var}/"
+      s.change_make_var! "GAME_USER", "`whoami`"
+    end
+    system "make", "all"
+
+    # for some reason, these aren't created automatically
+    bin.mkpath
+    var.mkpath
+
+    system "make", "install"
+
+    # the makefile doesn't install the manpage
+    man6.install "bastet.6"
+  end
+end

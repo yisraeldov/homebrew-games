0341c4c3f0b6457e824866c36be791882b7f5fd8
mbcoguno <mbcoguno@users.noreply.github.com>
2014-01-03 16:20:26 +0800

    mame & rom-tools 0.153
    * Add new rom-tools' tools.
    * Prevent ldplayer build nonexistent library.
    
    Closes #91.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- mame.rb
+++ mame.rb
@@ -1,27 +1,27 @@
-require 'formula'
+require "formula"
 
 class Mame < Formula
-  homepage 'http://mamedev.org/'
-  url 'svn://dspnet.fr/mame/trunk', :revision => '24476'
-  version '0.149u1'
+  homepage "http://mamedev.org/"
+  url "svn://dspnet.fr/mame/trunk", :revision => "29406"
+  version "0.153"
 
-  head 'svn://dspnet.fr/mame/trunk'
+  head "svn://dspnet.fr/mame/trunk"
 
   depends_on :x11
-  depends_on 'sdl'
+  depends_on "sdl"
 
   def install
-    ENV['MACOSX_USE_LIBSDL'] = '1'
-    ENV['INCPATH'] = "-I#{MacOS::X11.include}"
-    ENV['PTR64'] = (MacOS.prefer_64_bit? ? '1' : '0')
+    ENV["MACOSX_USE_LIBSDL"] = "1"
+    ENV["INCPATH"] = "-I#{MacOS::X11.include}"
+    ENV["PTR64"] = (MacOS.prefer_64_bit? ? "1" : "0")
 
-    system 'make', "CC=#{ENV.cc}", "LD=#{ENV.cxx}",
-                   'TARGET=mame', 'SUBTARGET=mame'
+    system "make", "CC=#{ENV.cc}", "LD=#{ENV.cxx}",
+                   "TARGET=mame", "SUBTARGET=mame"
 
     if MacOS.prefer_64_bit?
-      bin.install 'mame64' => 'mame'
+      bin.install "mame64" => "mame"
     else
-      bin.install 'mame'
+      bin.install "mame"
     end
   end
 end
--- rom-tools.rb
+++ rom-tools.rb
@@ -1,32 +1,59 @@
-require 'formula'
+require "formula"
 
 class RomTools < Formula
-  homepage 'http://mamedev.org/'
-  url 'svn://dspnet.fr/mame/trunk', :revision => '24476'
-  version '0.149u1'
+  homepage "http://mamedev.org/"
+  url "svn://dspnet.fr/mame/trunk", :revision => "29406"
+  version "0.153"
 
-  head 'svn://dspnet.fr/mame/trunk'
+  head "svn://dspnet.fr/mame/trunk"
 
   depends_on :x11
-  depends_on 'sdl'
+  depends_on "sdl"
+
+  # Keep bus part in separate library can make visual studio builds possible,
+  # but it cause ldplayer build failed, since it doesn't build bus library.
+  patch :DATA
 
   def install
-    ENV['MACOSX_USE_LIBSDL'] = '1'
-    ENV['INCPATH'] = "-I#{MacOS::X11.include}"
-    ENV['PTR64'] = (MacOS.prefer_64_bit? ? '1' : '0')
+    ENV["MACOSX_USE_LIBSDL"] = "1"
+    ENV["INCPATH"] = "-I#{MacOS::X11.include}"
+    ENV["PTR64"] = (MacOS.prefer_64_bit? ? "1" : "0")
 
-    system 'make', "CC=#{ENV.cc}", "LD=#{ENV.cxx}", 'tools'
-    system 'make', "CC=#{ENV.cc}", "LD=#{ENV.cxx}", 'TARGET=ldplayer'
+    system "make", "CC=#{ENV.cc}", "LD=#{ENV.cxx}", "tools"
+    system "make", "CC=#{ENV.cc}", "LD=#{ENV.cxx}", "TARGET=ldplayer"
 
     bin.install %W[
-      chdman jedutil ldresample ldverify regrep romcmp src2html srcclean
-      testkeys unidasm
+      chdman jedutil ldresample ldverify nltool pngcmp regrep romcmp src2html
+      srcclean testkeys unidasm
     ]
-    bin.install 'split' => 'rom-split'
+    bin.install "split" => "rom-split"
     if MacOS.prefer_64_bit?
-      bin.install 'ldplayer64' => 'ldplayer'
+      bin.install "ldplayer64" => "ldplayer"
     else
-      bin.install 'ldplayer'
+      bin.install "ldplayer"
     end
   end
 end
+
+__END__
+diff --git a/makefile b/makefile
+index 47db5dc..8e22b9d 100644
+--- a/makefile
++++ b/makefile
+@@ -678,7 +678,6 @@
+
+ LIBEMU = $(OBJ)/libemu.a
+ LIBOPTIONAL = $(OBJ)/$(TARGET)/$(SUBTARGET)/liboptional.a
+-LIBBUS = $(OBJ)/$(TARGET)/$(SUBTARGET)/libbus.a
+ LIBDASM = $(OBJ)/$(TARGET)/$(SUBTARGET)/libdasm.a
+ LIBUTIL = $(OBJ)/libutil.a
+ LIBOCORE = $(OBJ)/libocore.a
+@@ -871,7 +870,7 @@
+
+ ifndef EXECUTABLE_DEFINED
+
+-$(EMULATOR): $(EMUINFOOBJ) $(DRIVLISTOBJ) $(DRVLIBS) $(LIBOSD) $(LIBBUS) $(LIBOPTIONAL) $(LIBEMU) $(LIBDASM) $(LIBUTIL) $(EXPAT) $(SOFTFLOAT) $(JPEG_LIB) $(FLAC_LIB) $(7Z_LIB) $(FORMATS_LIB) $(LUA_LIB) $(WEB_LIB) $(ZLIB) $(LIBOCORE) $(MIDI_LIB) $(RESFILE)
++$(EMULATOR): $(EMUINFOOBJ) $(DRIVLISTOBJ) $(DRVLIBS) $(LIBOSD) $(LIBOPTIONAL) $(LIBEMU) $(LIBDASM) $(LIBUTIL) $(EXPAT) $(SOFTFLOAT) $(JPEG_LIB) $(FLAC_LIB) $(7Z_LIB) $(FORMATS_LIB) $(LUA_LIB) $(WEB_LIB) $(ZLIB) $(LIBOCORE) $(MIDI_LIB) $(RESFILE)
+ 	$(CC) $(CDEFS) $(CFLAGS) -c $(SRC)/version.c -o $(VERSIONOBJ)
+ 	@echo Linking $@...
+ 	$(LD) $(LDFLAGS) $(LDFLAGSEMULATOR) $(VERSIONOBJ) $^ $(LIBS) -o $@

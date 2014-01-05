7abbf2dad0cd3e52f2ea2fb9980556617cfea529
Tim Visher <tvisher@monetate.com>
2014-01-05 17:10:17 -0500

    mupen64plus 2.0.0
    Closes #92.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- mupen64plus.rb
+++ mupen64plus.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Mupen64plus < Formula
-  url 'http://mupen64plus.googlecode.com/files/mupen64plus-bundle-src-1.99.5.tar.gz'
+  url 'https://mupen64plus.googlecode.com/files/mupen64plus-bundle-src-2.0.tar.gz'
   homepage 'http://code.google.com/p/mupen64plus/'
-  sha1 'ca80ae446c9591e272e3ec93f0a2a8b01cfcd34e'
+  sha1 '078a518e1b162bd88993e1815967fdafe61d9e28'
 
   option 'disable-osd', 'Disables the On Screen Display'
   option 'enable-new-dynarec', 'Replace dynamic recompiler with Ari64\'s experimental dynarec'
@@ -16,21 +16,6 @@ class Mupen64plus < Formula
   depends_on :freetype unless build.include? 'disable-osd'
   depends_on 'libsamplerate' if build.include? 'with-src'
   depends_on 'speex' if build.include? 'with-speex'
-
-  def patches
-    [
-      # Current versions of mupen64plus do not compile on OSX with Xcode 4.3+
-      # due to Xcode 4.3 storing the SDKs in a different location.
-      # Already merged in the source tree, and will be fixed in the next version.
-      "https://gist.github.com/raw/4445512/0123c67cc020aba34aa5f2b256c848123f83549e/mupen64plus-1.99.5-osx-makefiles-patch.diff",
-      # Fixes improper inclusion of SDL and OpenGL.
-      # Already merged in the source tree, and will be fixed in the next version.
-      "https://gist.github.com/raw/4445589/e084b3465a66d58b974df3fd47e760521dd2bcad/mupen64plus-1.99.5-osx-uiconsole-sdlfix.diff",
-      # Fixup for the mupen64plus-core Makefile to use the system zlib
-      # For more see: http://code.google.com/p/mupen64plus/issues/detail?id=540
-      "https://gist.github.com/raw/4581659/8cab69edda633dac4a1e9fe520f7203c0b65ede1/mupen64plus-1.99.5-osx-zlib-fix.diff"
-    ]
-  end
 
   def install
     commonArgs = ["install", "PREFIX=#{prefix}", "INSTALL_STRIP_FLAG=-S"]

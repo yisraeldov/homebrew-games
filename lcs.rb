1721e736b5984077906cb52cdb882050ecec6cc7
Diogo Ramos <somar.ogoid@hotmail.com>
2014-02-13 21:46:45 -0200

    lcs 4.07.4b
    Closes #104.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

---
+++ lcs.rb
@@ -0,0 +1,18 @@
+require "formula"
+
+class Lcs < Formula
+  homepage "http://sourceforge.net/projects/lcsgame/"
+  url "svn://svn.code.sf.net/p/lcsgame/code/trunk", :revision => '738'
+  version '4.07.4b'
+
+  head "svn://svn.code.sf.net/p/lcsgame/code/trunk"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+
+  def install
+    system './bootstrap'
+    system "./configure", "LIBS=-liconv", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+end

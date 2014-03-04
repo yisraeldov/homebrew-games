f88e9b119b643e3916c95eac11ee329f4147d7c7
Jack Nagel <jacknagel@gmail.com>
2014-03-04 10:15:38 -0600

    frobtabs: use head block

--- frobtads.rb
+++ frobtads.rb
@@ -4,9 +4,10 @@ class Frobtads < Formula
   homepage 'http://www.tads.org/frobtads.htm'
   url 'http://www.tads.org/frobtads/frobtads-1.2.3.tar.gz'
   sha1 '330217c0b6ee298bf9db986bc4ce84b86aa5e3e2'
-  head 'git://git.assembla.com/frobtads.git'
 
-  if build.head?
+  head do
+    url 'git://git.assembla.com/frobtads.git'
+
     depends_on :autoconf
     depends_on :automake
     depends_on :libtool

1ac6c784982b777567eb95310fe220793e504820
Adam Vandenberg <flangy@gmail.com>
2014-03-14 20:35:41 -0700

    open-tyrian 2.1.20130907

--- open-tyrian.rb
+++ open-tyrian.rb
@@ -1,24 +1,24 @@
 require 'formula'
 
 class OpenTyrian < Formula
-  url 'http://opentyrian.googlecode.com/hg/', :revision => '9ddcd06e48'
-  homepage 'http://code.google.com/p/opentyrian/'
-  version '20091122'
+  homepage "http://code.google.com/p/opentyrian/"
+  url "http://www.camanis.net/opentyrian/releases/opentyrian-2.1.20130907-src.tar.gz"
+  sha1 "c5b97aea3931db6889acb639e59e619162b05183"
 
-  depends_on 'sdl'
-  depends_on 'sdl_net'
+  depends_on "sdl"
+  depends_on "sdl_net"
 
-  resource 'data' do
-    url 'http://sites.google.com/a/camanis.net/opentyrian/tyrian/tyrian21.zip'
-    sha1 '29827de99c92cdba7ac8d8b1307776c8f473cd44'
+  resource "data" do
+    url "http://sites.google.com/a/camanis.net/opentyrian/tyrian/tyrian21.zip"
+    sha1 "29827de99c92cdba7ac8d8b1307776c8f473cd44"
   end
 
   def install
-    libexec.install resource('data')
+    libexec.install resource("data")
 
     system "make release"
     libexec.install "opentyrian"
-    (bin+'opentyrian').write <<-END.undent
+    (bin/"opentyrian").write <<-END.undent
       #!/bin/bash
       "#{libexec}/opentyrian" --data="#{libexec}" "$@"
     END

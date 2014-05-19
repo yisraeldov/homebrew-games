002fe74763e92052f3da9b140942b53ebebd0a0e
Misty De Meo <mistydemeo@gmail.com>
2014-05-19 16:57:24 -0700

    mednafen 0.9.34.1

--- mednafen.rb
+++ mednafen.rb
@@ -2,11 +2,8 @@ require 'formula'
 
 class Mednafen < Formula
   homepage 'http://mednafen.sourceforge.net/'
-  url 'http://forum.fobby.net/index.php?t=getfile&id=572'
-  sha1 'aa57533321b79ae63f5a64e2bbbdb31a397902d3'
-  version '0.9.32.1-WIP'
-
-  option 'with-snes', 'Build with SNES support'
+  url 'https://prdownloads.sourceforge.net/mednafen/mednafen-0.9.34.1.tar.bz2'
+  sha1 'ee3d2b2afb3a4377b4d48513ba9c9048f5530690'
 
   depends_on 'pkg-config' => :build
   depends_on 'sdl'
@@ -14,14 +11,7 @@ class Mednafen < Formula
   depends_on 'libsndfile'
 
   def install
-    args = %W[--prefix=#{prefix} --disable-dependency-tracking]
-    args << "--disable-snes" if ENV.compiler == :clang
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
     system "make install"
-  end
-
-  def caveats; <<-EOS.undent
-    The SNES module will not be compiled if you use clang as the compiler.
-    EOS
   end
 end

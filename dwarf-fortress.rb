f5af9f32ef74323e706fe91f2a35e6f2fadcb810
Misty De Meo <mistydemeo@gmail.com>
2014-07-13 13:13:57 -0700

    dwarf-fprtress 0.40.03

--- dwarf-fortress.rb
+++ dwarf-fortress.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class DwarfFortress < Formula
   homepage 'http://www.bay12games.com/dwarves/'
-  url 'http://www.bay12games.com/dwarves/df_40_02_osx.tar.bz2'
-  version '0.40.02'
-  sha1 '69e69de5b837e249c67f26131258528036d485c7'
+  url 'http://www.bay12games.com/dwarves/df_40_03_osx.tar.bz2'
+  version '0.40.03'
+  sha1 '44e71c9cddb5deddc38dac13d52d91cd487054cd'
 
   def install
     (bin+'dwarffortress').write <<-EOS.undent

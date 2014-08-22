e2fc639f1035bab2e7c18c41142f6f6636997b2d
Misty De Meo <mistydemeo@gmail.com>
2014-08-21 22:44:37 -0700

    libtcod: remove X11 checks
    Fixes #139.

--- libtcod.rb
+++ libtcod.rb
@@ -8,6 +8,9 @@ class Libtcod < Formula
   depends_on 'cmake' => :build
   depends_on 'sdl'
 
+  # Remove unnecessary X11 check - our SDL doesn't use X11
+  patch :DATA
+
   def install
     system "cmake", ".", *std_cmake_args
     system "make"
@@ -20,3 +23,25 @@ class Libtcod < Formula
     libexec.install 'data'
   end
 end
+
+__END__
+diff --git a/CMakeLists.txt b/CMakeLists.txt
+index a573528..e88419b 100644
+--- a/CMakeLists.txt
++++ b/CMakeLists.txt
+@@ -57,7 +57,6 @@ if(MSVC OR MINGW)
+ 	LINK_DIRECTORIES(${DEPENDENCY_DIR}/SDL-1.2.15/lib/${LIB_DIR}/ 
+ 					${DEPENDENCY_DIR}/zlib-1.2.3/lib/${ZLIB_DIR}/)
+ ELSE()
+-	find_package(X11 REQUIRED)
+ 	find_package(SDL REQUIRED)
+ 	find_package(ZLIB REQUIRED)
+ 	find_package(Threads REQUIRED)
+@@ -72,7 +71,6 @@ ELSE()
+ 			      ${SDL_INCLUDE_DIR} # again, singular...
+ 			      ${ZLIB_INCLUDE_DIRS}
+ 			      ${M_LIB_INCLUDE_DIRS}
+-			      ${X11_INCLUDE_DIRS}
+ 			      ${OPENGL_INCLUDE_DIRS})
+ 	IF(CMAKE_BUILD_TYPE MATCHES "Debug")
+ 		find_library(EFENCE_LIBRARY efence PATHS /usr/lib /usr/local/lib)

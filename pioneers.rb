f0ad617ae92a7380e656f72b378a29b428315b2a
Misty De Meo <mistydemeo@gmail.com>
2014-09-20 01:21:53 -0700

    pioneers: cleanups

--- pioneers.rb
+++ pioneers.rb
@@ -1,22 +1,22 @@
-require 'formula'
+require "formula"
 
 class Pioneers < Formula
-  url 'https://downloads.sourceforge.net/project/pio/Source/pioneers-15.1.tar.gz'
-  homepage 'http://pio.sourceforge.net/'
-  sha1 'cea94cd77edef31b3f9e601077dff9b199dfeaf4'
+  homepage "http://pio.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/pio/Source/pioneers-15.1.tar.gz"
+  sha1 "cea94cd77edef31b3f9e601077dff9b199dfeaf4"
 
   fails_with :clang do
     build 318
     cause "'#line directive requires a positive integer' argument in generated file"
   end
 
-  depends_on 'pkg-config' => :build
-  depends_on 'intltool' => :build
-  depends_on 'gettext'
-  depends_on 'gtk+'
-  depends_on 'gdk-pixbuf'
-  depends_on 'librsvg' # svg images for gdk-pixbuf
-  depends_on 'hicolor-icon-theme'
+  depends_on "pkg-config" => :build
+  depends_on "intltool" => :build
+  depends_on "gettext"
+  depends_on "gtk+"
+  depends_on "gdk-pixbuf"
+  depends_on "librsvg" # svg images for gdk-pixbuf
+  depends_on "hicolor-icon-theme"
 
   def install
     # fix usage of echo options not supported by sh

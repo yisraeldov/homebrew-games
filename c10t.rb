d904b6b5a63703504ad750e05604b70da4652050
Misty De Meo <mistydemeo@gmail.com>
2014-09-19 20:41:40 -0700

    c10t: fix freetype detection, libpng usage
    Fixes #115.

--- c10t.rb
+++ c10t.rb
@@ -7,6 +7,7 @@ class C10t < Formula
 
   depends_on 'cmake' => :build
   depends_on 'boost'
+  depends_on 'freetype'
 
   # Needed to compile against newer boost
   # Can be removed for next version of c10t after 1.7
@@ -14,6 +15,19 @@ class C10t < Formula
   patch do
     url "https://github.com/udoprog/c10t/commit/4a392b9f06d08c70290f4c7591e84ecdbc73d902.diff"
     sha1 "e528aef909138a596ae0c7a48e84593b816a27f0"
+  end
+
+  # Fix freetype detection; adapted from this upstream commit:
+  # https://github.com/udoprog/c10t/commit/2a2b8e49d7ed4e51421cc71463c1c2404adc6ab1
+  patch do
+    url "https://gist.githubusercontent.com/mistydemeo/f7ab02089c43dd557ef4/raw/a0ae7974e635b8ebfd02e314cfca9aa8dc95029d/c10t-freetype.diff"
+    sha1 "088ba840b8f636aae62fd70cc32b963fedadf233"
+  end
+
+  # Ensure zlib header is included for libpng; fixed upstream
+  patch do
+    url "https://github.com/udoprog/c10t/commit/800977bb23e6b4f9da3ac850ac15dd216ece0cda.diff"
+    sha1 "706fc266f7ba064c3414f3ab6fef90301386cff0"
   end
 
   def install

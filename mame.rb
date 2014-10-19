831f3d8b6b07508448a29fae24981c6f080f8e5b
Parry <mbcoguno@users.noreply.github.com>
2014-10-20 07:03:31 +0800

    mame & rom-tools 0.155
    * Use tarball.
    * Enable LTO if able.
    * Remove rom-tools' ldplayer patch.
    
    Closes #154.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- mame.rb
+++ mame.rb
@@ -2,16 +2,18 @@ require "formula"
 
 class Mame < Formula
   homepage "http://mamedev.org/"
-  url "svn://dspnet.fr/mame/trunk", :revision => "31397"
-  version "0.154"
+  url "https://github.com/mamedev/mame/archive/mame0155.tar.gz"
+  sha1 "0e56d53dd6dd916b3c29387112a7042befc501bd"
+  version "0.155"
 
-  head "svn://dspnet.fr/mame/trunk"
+  head "https://github.com/mamedev/mame.git"
 
   depends_on "sdl"
 
   def install
     ENV["MACOSX_USE_LIBSDL"] = "1"
     ENV["PTR64"] = (MacOS.prefer_64_bit? ? "1" : "0")
+    ENV["LTO"] = "1" if ENV.compiler == :clang or ENV.compiler =~ /^gcc-(4\.[6-9])$/
 
     system "make", "CC=#{ENV.cc}", "LD=#{ENV.cxx}",
                    "TARGET=mame", "SUBTARGET=mame"
--- rom-tools.rb
+++ rom-tools.rb
@@ -2,20 +2,18 @@ require "formula"
 
 class RomTools < Formula
   homepage "http://mamedev.org/"
-  url "svn://dspnet.fr/mame/trunk", :revision => "31397"
-  version "0.154"
+  url "https://github.com/mamedev/mame/archive/mame0155.tar.gz"
+  sha1 "0e56d53dd6dd916b3c29387112a7042befc501bd"
+  version "0.155"
 
-  head "svn://dspnet.fr/mame/trunk"
+  head "https://github.com/mamedev/mame.git"
 
   depends_on "sdl"
-
-  # Keep bus part in separate library can make visual studio builds possible,
-  # but it cause ldplayer build failed, since it doesn't build bus library.
-  patch :DATA
 
   def install
     ENV["MACOSX_USE_LIBSDL"] = "1"
     ENV["PTR64"] = (MacOS.prefer_64_bit? ? "1" : "0")
+    ENV["LTO"] = "1" if ENV.compiler == :clang or ENV.compiler =~ /^gcc-(4\.[6-9])$/
 
     system "make", "CC=#{ENV.cc}", "LD=#{ENV.cxx}", "tools"
     system "make", "CC=#{ENV.cc}", "LD=#{ENV.cxx}", "TARGET=ldplayer"
@@ -32,26 +30,3 @@ class RomTools < Formula
     end
   end
 end
-
-__END__
-diff --git a/makefile b/makefile
-index 47db5dc..8e22b9d 100644
---- a/makefile
-+++ b/makefile
-@@ -678,7 +678,6 @@
-
- LIBEMU = $(OBJ)/libemu.a
- LIBOPTIONAL = $(OBJ)/$(TARGET)/$(SUBTARGET)/liboptional.a
--LIBBUS = $(OBJ)/$(TARGET)/$(SUBTARGET)/libbus.a
- LIBDASM = $(OBJ)/$(TARGET)/$(SUBTARGET)/libdasm.a
- LIBUTIL = $(OBJ)/libutil.a
- LIBOCORE = $(OBJ)/libocore.a
-@@ -871,7 +870,7 @@
-
- ifndef EXECUTABLE_DEFINED
-
--$(EMULATOR): $(EMUINFOOBJ) $(DRIVLISTOBJ) $(DRVLIBS) $(LIBOSD) $(LIBBUS) $(LIBOPTIONAL) $(LIBEMU) $(LIBDASM) $(LIBUTIL) $(EXPAT) $(SOFTFLOAT) $(JPEG_LIB) $(FLAC_LIB) $(7Z_LIB) $(FORMATS_LIB) $(LUA_LIB) $(SQLITE3_LIB) $(WEB_LIB) $(ZLIB) $(LIBOCORE) $(MIDI_LIB) $(RESFILE)
-+$(EMULATOR): $(EMUINFOOBJ) $(DRIVLISTOBJ) $(DRVLIBS) $(LIBOSD) $(LIBOPTIONAL) $(LIBEMU) $(LIBDASM) $(LIBUTIL) $(EXPAT) $(SOFTFLOAT) $(JPEG_LIB) $(FLAC_LIB) $(7Z_LIB) $(FORMATS_LIB) $(LUA_LIB) $(SQLITE3_LIB) $(WEB_LIB) $(ZLIB) $(LIBOCORE) $(MIDI_LIB) $(RESFILE)
- 	$(CC) $(CDEFS) $(CFLAGS) -c $(SRC)/version.c -o $(VERSIONOBJ)
- 	@echo Linking $@...
- 	$(LD) $(LDFLAGS) $(LDFLAGSEMULATOR) $(VERSIONOBJ) $^ $(LIBS) -o $@

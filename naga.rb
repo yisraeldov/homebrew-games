06c55d2c7123bfeb1bf0964c89d96750f33e4ae1
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-23 21:53:23 +0000

    naga: add 1.0 bottle.

--- naga.rb
+++ naga.rb
@@ -5,6 +5,13 @@ class Naga < Formula
   url "https://github.com/anayjoshi/naga/archive/naga-v1.0.tar.gz"
   sha1 "9ba4aaf827521222b4b43fb736614c4c7b7bfc8e"
 
+  bottle do
+    cellar :any
+    sha1 "6ca134f2efac554b07e770139c3c33f4df6feae6" => :yosemite
+    sha1 "530bf68d68d32def280055ca3c07884b24c9538d" => :mavericks
+    sha1 "89f602eae5136fb53ec8e816ae384ac8a181b1a1" => :mountain_lion
+  end
+
   def install
     bin.mkpath()
     system "make", "install", "INSTALL_PATH=#{bin}/naga"

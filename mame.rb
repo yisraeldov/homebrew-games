a41eb71bc06a59a814b102de98621100a7b1d8ec
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-24 18:29:22 +0000

    mame: add 0.155 bottle.
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- mame.rb
+++ mame.rb
@@ -8,6 +8,14 @@ class Mame < Formula
 
   head "https://github.com/mamedev/mame.git"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/games"
+    cellar :any
+    sha1 "c9198093fd5ca8d90313f471277c8297c68321be" => :yosemite
+    sha1 "e712fc84e2b971121d24f895057bec56457b687f" => :mavericks
+    sha1 "f612228ac79d3987de4e0062751e98111c3e095c" => :mountain_lion
+  end
+
   depends_on "sdl"
 
   def install

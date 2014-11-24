36f649de8e716f5a5c62a236948ab9c89ddc3029
BrewTestBot <brew-test-bot@googlegroups.com>
2014-11-24 19:08:51 +0000

    dosbox: add 0.74 bottle.
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- dosbox.rb
+++ dosbox.rb
@@ -5,6 +5,14 @@ class Dosbox < Formula
   url 'https://downloads.sourceforge.net/project/dosbox/dosbox/0.74/dosbox-0.74.tar.gz'
   sha1 '2d99f0013350efb29b769ff19ddc8e4d86f4e77e'
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/games"
+    cellar :any
+    sha1 "c705acee92739e73802870940c377a1d25f2d3ca" => :yosemite
+    sha1 "f19db38fe5ed10d2ce74c84835548d3725313e8e" => :mavericks
+    sha1 "a5d26fbb678ab9b68d35a23f9296fd39a8587d74" => :mountain_lion
+  end
+
   depends_on :libpng
   depends_on 'sdl'
   depends_on 'sdl_net'

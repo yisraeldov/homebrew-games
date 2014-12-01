66f0876741ed76bbd521bb4bc38507c387c41155
Parry <mbcoguno@users.noreply.github.com>
2014-12-01 14:30:46 +0800

    mame & rom-tools 0.156
    * Switch to SDL2
    
    Closes #181.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- rom-tools.rb
+++ rom-tools.rb
@@ -2,13 +2,18 @@ require "formula"
 
 class RomTools < Formula
   homepage "http://mamedev.org/"
-  url "https://github.com/mamedev/mame/archive/mame0155.tar.gz"
-  sha1 "9f4e92491bf56f2ea76f136daa69dbf88e589b71"
-  version "0.155"
+  url "https://github.com/mamedev/mame/archive/mame0156.tar.gz"
+  sha1 "9cda662385c0b168ca564dab0fb1e839065f6a01"
+  version "0.156"
 
   head "https://github.com/mamedev/mame.git"
 
-  depends_on "sdl"
+  depends_on "sdl2"
+
+  # Fix build with non-framework SDL 2 issue on OS X.
+  # Linking Cocoa's library. Don't hard coding SDL execution name.
+  # Upstream: <https://github.com/mamedev/mame/pull/60>
+  patch :DATA
 
   def install
     ENV["MACOSX_USE_LIBSDL"] = "1"
@@ -30,3 +35,20 @@ class RomTools < Formula
     end
   end
 end
+
+__END__
+diff --git a/src/osd/sdl/sdl.mak b/src/osd/sdl/sdl.mak
+index 21dd814..eaec664 100644
+--- a/src/osd/sdl/sdl.mak
++++ b/src/osd/sdl/sdl.mak
+@@ -507,8 +507,8 @@ else
+ # files (header files are #include "SDL/something.h", so the extra "/SDL"
+ # causes a significant problem)
+-INCPATH += `sdl-config --cflags | sed 's:/SDL::'`
++INCPATH += `$(SDL_CONFIG) --cflags | sed 's:/SDL::'`
+ CCOMFLAGS += -DNO_SDL_GLEXT
+ # Remove libSDLmain, as its symbols conflict with SDLMain_tmpl.m
+-LIBS += `sdl-config --libs | sed 's/-lSDLmain//'` -lpthread -framework OpenGL
++LIBS += `$(SDL_CONFIG) --libs | sed 's/-lSDLmain//'` -lpthread -framework Cocoa -framework OpenGL
+ DEFS += -DMACOSX_USE_LIBSDL
+ endif   # MACOSX_USE_LIBSDL

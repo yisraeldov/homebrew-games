c51ff47f019bb9823876cbe5d2678573aae2641d
Jack Nagel <jacknagel@gmail.com>
2014-12-05 01:20:34 -0500

    dosbox: modernize deps

--- dosbox.rb
+++ dosbox.rb
@@ -5,7 +5,7 @@ class Dosbox < Formula
   url 'https://downloads.sourceforge.net/project/dosbox/dosbox/0.74/dosbox-0.74.tar.gz'
   sha1 '2d99f0013350efb29b769ff19ddc8e4d86f4e77e'
 
-  depends_on :libpng
+  depends_on "libpng"
   depends_on 'sdl'
   depends_on 'sdl_net'
   depends_on 'sdl_sound'

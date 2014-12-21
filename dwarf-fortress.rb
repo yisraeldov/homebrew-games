4d726b0ad8dc2181848929e8d57484372aaca53d
lethosor <lethosor@gmail.com>
2014-12-21 14:57:20 -0500

    dwarf-fortress 0.40.22
    Closes #194.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- dwarf-fortress.rb
+++ dwarf-fortress.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class DwarfFortress < Formula
   homepage 'http://www.bay12games.com/dwarves/'
-  url 'http://www.bay12games.com/dwarves/df_40_21_osx.tar.bz2'
-  version '0.40.21'
-  sha1 '84b3797fc960c1a7232babdc991a91c050694dd8'
+  url 'http://www.bay12games.com/dwarves/df_40_22_osx.tar.bz2'
+  version '0.40.22'
+  sha1 '421782462179a510d6f7a3daa79d1082a2be2ac7'
 
   def install
     (bin+'dwarffortress').write <<-EOS.undent

09a44bd9fdc2bdd7266575a01f514eff54716215
Pavel Puchkin <i@neoascetic.me>
2014-12-27 11:16:20 +0200

    Minetest 0.4.11
    Closes #198.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- minetest.rb
+++ minetest.rb
@@ -27,28 +27,11 @@ class Minetest < Formula
   end
 
   stable do
-    url "https://github.com/minetest/minetest/archive/0.4.10.tar.gz"
-    sha1 "06179ea793200236254105c5bc656c1e2e1fb1ef"
+    url "https://github.com/minetest/minetest/archive/0.4.11.tar.gz"
+    sha1 "8af33253e0ac3a2c43d8127462e51461f9761343"
 
     resource "minetest_game" do
-      url "https://github.com/minetest/minetest_game.git", :tag => "0.4.10"
-    end
-
-    # these patches wasn't backported to 0.4.10, so keep it until new stable
-    patch do
-      url "https://github.com/minetest/minetest/pull/1928.diff"
-      sha1 "0640b82417a44ed1aeeb7dc8649184fe14e95e34"
-    end
-
-    patch do
-      url "https://github.com/minetest/minetest/pull/1975.diff"
-      sha1 "f867a9f73b046b23a1c3dd0e97c104cc98a66ea1"
-    end
-
-    # related to patch above, as resource because binary file cannot be patched :(
-    resource "icns" do
-      url "https://raw.githubusercontent.com/neoascetic/minetest/3b902fd574bfba74ebbd58369dce74162385b410/misc/minetest-icon.icns"
-      sha1 "66a1fa2c2ecc3520b6b4b23621d739aff971ce76"
+      url "https://github.com/minetest/minetest_game.git", :tag => "0.4.11"
     end
   end
 
@@ -62,7 +45,6 @@ class Minetest < Formula
 
   def install
     (buildpath/"games/minetest_game").install resource("minetest_game")
-    (buildpath/"misc").install resource("icns") if build.stable?
 
     args = std_cmake_args - %w{-DCMAKE_BUILD_TYPE=None}
     args << '-DCMAKE_BUILD_TYPE=Release'

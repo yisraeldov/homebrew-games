dc78d5da08b9388ea68d1868a6c45dd915ff817b
Misty De Meo <mistydemeo@gmail.com>
2014-12-27 18:39:02 -0800

    nethack4: unify the two stable blocks

--- nethack4.rb
+++ nethack4.rb
@@ -2,31 +2,12 @@ require "formula"
 
 class Nethack4 < Formula
   homepage "http://nethack4.org"
-  url "http://nethack4.org/media/releases/nethack4-4.3.0-beta1-source.tar.gz"
-  sha1 "1f1b11cf5748b58926b354fae763e80df4f63372"
-  version "4.3.0-beta1"
-
-  head do
-    url "https://gitorious.org/nitrohack/ais523.git", :branch => "nicehack"
-
-    # yes, even though we build without tiles - see http://trac.nethack4.org/ticket/646
-    depends_on "libpng"
-  end
-
-  bottle do
-    root_url "https://downloads.sf.net/project/machomebrew/Bottles/games"
-    sha1 "bfe3bd2461289bb7064dd915128733e1692fce3c" => :yosemite
-    sha1 "225b12566eda278271735f5f737e210d0afba302" => :mavericks
-    sha1 "c8eb2ecd075280628e7c4e4f81afbf7afd83c477" => :mountain_lion
-  end
-
-  # Assumes C11 _Noreturn is available for clang:
-  # http://trac.nethack4.org/ticket/568
-  fails_with :clang do
-    build 425
-  end
 
   stable do
+    url "http://nethack4.org/media/releases/nethack4-4.3.0-beta1-source.tar.gz"
+    sha1 "1f1b11cf5748b58926b354fae763e80df4f63372"
+    version "4.3.0-beta1"
+
     # These two patches remove the use of realtime POSIX signals on OS X,
     # which doesn't provide that feature
     # http://trac.nethack4.org/ticket/514; fixed upstream, will be in next release
@@ -75,6 +56,26 @@ class Nethack4 < Formula
       url "https://gist.githubusercontent.com/mistydemeo/7a52c503ecef16ea8194/raw/58e28b36de5a0a75ed3ca0d6a2b1978241db8052/nethack4-prefix.patch"
       sha1 "4449fbd990a3423941d1547c4720ec6b0e4dc750"
     end
+  end
+
+  head do
+    url "https://gitorious.org/nitrohack/ais523.git", :branch => "nicehack"
+
+    # yes, even though we build without tiles - see http://trac.nethack4.org/ticket/646
+    depends_on "libpng"
+  end
+
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/games"
+    sha1 "bfe3bd2461289bb7064dd915128733e1692fce3c" => :yosemite
+    sha1 "225b12566eda278271735f5f737e210d0afba302" => :mavericks
+    sha1 "c8eb2ecd075280628e7c4e4f81afbf7afd83c477" => :mountain_lion
+  end
+
+  # Assumes C11 _Noreturn is available for clang:
+  # http://trac.nethack4.org/ticket/568
+  fails_with :clang do
+    build 425
   end
 
   def install

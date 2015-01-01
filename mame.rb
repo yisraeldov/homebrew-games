50253d08a8a812fc5ba6f33ad2219c07800ff5b0
Parry <mbcoguno@users.noreply.github.com>
2015-01-01 23:31:10 +0800

    mame 0.157
    * Style updates.
    * Add manpage.

--- mame.rb
+++ mame.rb
@@ -1,10 +1,8 @@
-require "formula"
-
 class Mame < Formula
   homepage "http://mamedev.org/"
-  url "https://github.com/mamedev/mame/archive/mame0156.tar.gz"
-  sha1 "9cda662385c0b168ca564dab0fb1e839065f6a01"
-  version "0.156"
+  url "https://github.com/mamedev/mame/archive/mame0157.tar.gz"
+  sha1 "cac1ccb4194715be63dd4d4754a575b9e1c11ea3"
+  version "0.157"
 
   head "https://github.com/mamedev/mame.git"
 
@@ -17,11 +15,6 @@ class Mame < Formula
   end
 
   depends_on "sdl2"
-
-  # Fix build with non-framework SDL 2 issue on OS X.
-  # Linking Cocoa's library. Don't hard coding SDL execution name.
-  # Upstream: <https://github.com/mamedev/mame/pull/60>
-  patch :DATA
 
   def install
     ENV["MACOSX_USE_LIBSDL"] = "1"
@@ -36,22 +29,6 @@ class Mame < Formula
     else
       bin.install "mame"
     end
+    man6.install "src/osd/sdl/man/mame.6"
   end
 end
-
-__END__
-diff --git a/src/osd/sdl/sdl.mak b/src/osd/sdl/sdl.mak
-index 21dd814..eaec664 100644
---- a/src/osd/sdl/sdl.mak
-+++ b/src/osd/sdl/sdl.mak
-@@ -507,8 +507,8 @@ else
- # files (header files are #include "SDL/something.h", so the extra "/SDL"
- # causes a significant problem)
--INCPATH += `sdl-config --cflags | sed 's:/SDL::'`
-+INCPATH += `$(SDL_CONFIG) --cflags | sed 's:/SDL::'`
- CCOMFLAGS += -DNO_SDL_GLEXT
- # Remove libSDLmain, as its symbols conflict with SDLMain_tmpl.m
--LIBS += `sdl-config --libs | sed 's/-lSDLmain//'` -lpthread -framework OpenGL
-+LIBS += `$(SDL_CONFIG) --libs | sed 's/-lSDLmain//'` -lpthread -framework Cocoa -framework OpenGL
- DEFS += -DMACOSX_USE_LIBSDL
- endif   # MACOSX_USE_LIBSDL

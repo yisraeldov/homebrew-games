376114e937f08860b7cd828ea17917cfe141c33d
Miles Crabill <miles@milescrabill.com>
2015-01-23 16:13:38 -0800

    fuego: update revision 1981
    older versions of fuego don't work with recent boost
    
    Closes #207.
    Closes #208.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- fuego.rb
+++ fuego.rb
@@ -1,14 +1,16 @@
-require 'formula'
-
 class Fuego < Formula
-  url 'https://downloads.sourceforge.net/project/fuego/fuego/1.0/fuego-1.0.tar.gz'
-  homepage 'http://fuego.sourceforge.net/'
-  sha1 '1fe3de726d7020278f7cb2f678a1909053bf0107'
+  homepage "http://fuego.sourceforge.net/"
+  head "http://svn.code.sf.net/p/fuego/code/trunk"
+  url "http://svn.code.sf.net/p/fuego/code/trunk", :revision => 1981
+  version "1.1.SVN"
 
+  # automake should be a build dep, but won't install otherwise
+  depends_on 'automake'
   depends_on 'boost'
 
   def install
-    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
-    system "make install"
+    system "autoreconf", "--force", "--install"
+    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
+    system "make", "install"
   end
 end

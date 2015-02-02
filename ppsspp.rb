b7913d8994e4bcaca714a9f7a2a025fe6ea6d301
Kyungdahm Yun <tomyun@gmail.com>
2015-02-01 20:34:09 -0800

    ppsspp 1.0
    Closes #210.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- ppsspp.rb
+++ ppsspp.rb
@@ -1,6 +1,6 @@
 class Ppsspp < Formula
   homepage "http://ppsspp.org/"
-  url "https://github.com/hrydgard/ppsspp.git", :tag => "v0.9.9.1"
+  url "https://github.com/hrydgard/ppsspp.git", :tag => "v1.0"
   head "https://github.com/hrydgard/ppsspp.git"
 
   bottle do
@@ -12,15 +12,8 @@ class Ppsspp < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on "sdl"
+  depends_on "sdl2"
   depends_on "ffmpeg"
-
-  # Fix audio playback with system-wide FFmpeg
-  # https://github.com/hrydgard/ppsspp/issues/5772
-  patch do
-    url "https://abf.rosalinux.ru/import/ppsspp/raw/rosa2014.1/ppsspp-0.9.9.1-ffmpeg2.2.patch"
-    sha1 "cfd3884471c5b1024a60b656f079f31100415571"
-  end
 
   def install
     # Build type will be set to "Release" by default
@@ -32,7 +25,7 @@ class Ppsspp < Formula
     args << "-DFFMPEG_BUILDDIR=#{ffmpeg.opt_prefix}"
     inreplace "CMakeLists.txt" do |s|
       s.gsub! "STATIC IMPORTED", "SHARED IMPORTED"
-      s.gsub! /\${FFMPEG_BUILDDIR}\/lib.*\/(lib.*)\.a/, "${FFMPEG_BUILDDIR}/lib/\\1.dylib"
+      s.gsub! %r{\${FFMPEG_BUILDDIR}\/lib.*\/(lib.*)\.a}, "${FFMPEG_BUILDDIR}/lib/\\1.dylib"
     end
 
     mkdir "build" do
@@ -41,5 +34,10 @@ class Ppsspp < Formula
       prefix.install "PPSSPPSDL.app"
       bin.write_exec_script "#{prefix}/PPSSPPSDL.app/Contents/MacOS/PPSSPPSDL"
     end
+  end
+
+  test do
+    # FIXME: implement a proper test when command arguments get supported
+    true
   end
 end

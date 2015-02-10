6ba4cd135296176afd375aad8f119d9c79b0b05e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-10 00:25:43 +0000

    mapcrafter: add 1.5.4 bottle.

--- mapcrafter.rb
+++ mapcrafter.rb
@@ -3,6 +3,14 @@ class Mapcrafter < Formula
   url "https://github.com/mapcrafter/mapcrafter/archive/v.1.5.4.tar.gz"
   sha1 "ffe7d17a57099506b572bb6866ea76c8ad5f0564"
 
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/games"
+    cellar :any
+    sha1 "ac8fd25b80cb3afda8e6efbdbea1a6d09a010771" => :yosemite
+    sha1 "23a458a7763f44d020ff5a5ef97152455573c607" => :mavericks
+    sha1 "a50194903b88e00f4550996c83f1c083dbaf2689" => :mountain_lion
+  end
+
   needs :cxx11
 
   depends_on "cmake" => :build

cff29f3808f2ccf12e71d88e341b6ad50f6e6f8f
Jack Nagel <jacknagel@gmail.com>
2015-02-23 00:56:15 -0500

    homeworlds 20141022
    Closes #113.

--- homeworlds.rb
+++ homeworlds.rb
@@ -1,16 +1,13 @@
-require 'formula'
-
 class Homeworlds < Formula
-  homepage 'https://github.com/Quuxplusone/Homeworlds/'
-  url 'https://github.com/Quuxplusone/Homeworlds.git',
-    :revision => 'd0543623d21663d075c64ba050f2eef54f679f5e'
-  version '20120723'
+  homepage "https://github.com/Quuxplusone/Homeworlds/"
+  url "https://github.com/Quuxplusone/Homeworlds.git",
+    :revision => "917cd7e7e6d0a5cdfcc56cd69b41e3e80b671cde"
+  version "20141022"
 
-  depends_on 'wxmac'
+  depends_on "wxmac"
 
   def install
-    system 'make'
-    bin.install "wxgui" => "homeworlds-wx"
-    bin.install "annotate" => "homeworlds-cli"
+    system "make"
+    bin.install "wxgui" => "homeworlds-wx", "annotate" => "homeworlds-cli"
   end
 end

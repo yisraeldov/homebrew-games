24b16d5f81e2943dcd3e54cc40249c0cfb4908ef
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-23 22:31:01 +0000

    residualvm: add 0.2.1 bottle.

--- residualvm.rb
+++ residualvm.rb
@@ -4,6 +4,13 @@ class Residualvm < Formula
   sha1 "b39b14678b87ebd917d91b1a6e411446ac5cfef4"
   head "https://github.com/residualvm/residualvm.git"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-games"
+    sha1 "564a86dacaad4f809686f111ea7ca9fc342ccf39" => :yosemite
+    sha1 "2037f7546c6f07b663b0433fed05c1190ec0a519" => :mavericks
+    sha1 "493c255b9e041085624e3e2570f7cbe29547654b" => :mountain_lion
+  end
+
   depends_on "sdl"
 
   def install

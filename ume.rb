c38cdc684ae14becc46c7565b492c6467c2e3720
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-24 21:14:13 -0500

    ume: update 0.158 bottle.

--- ume.rb
+++ ume.rb
@@ -7,11 +7,10 @@ class Ume < Formula
   head "https://github.com/mamedev/mame.git"
 
   bottle do
-    root_url "https://downloads.sf.net/project/machomebrew/Bottles/games"
+    root_url "https://homebrew.bintray.com/bottles-games"
     cellar :any
-    sha1 "7ebb3549d4da3bd2eb8dd7b7664e07080905db21" => :yosemite
-    sha1 "b5b99840681e125a96e8d3e29c7ff884720b3732" => :mavericks
-    sha1 "25b44824b5f6fcdd515f7ad6b12ef81a5b2f5a75" => :mountain_lion
+    sha1 "61b3ca6ad1c5a780e5f716d4b13f5e39d523b69f" => :mavericks
+    sha1 "1c1793d00682bbaa95e23009ecf0373f74ea3b1d" => :mountain_lion
   end
 
   depends_on "sdl2"

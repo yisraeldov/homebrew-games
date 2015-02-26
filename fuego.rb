b9e7fcbd42e5a12aad384956ceee61f5422d0202
BrewTestBot <brew-test-bot@googlegroups.com>
2015-02-26 03:34:50 +0000

    fuego: add 1.1.SVN bottle.

--- fuego.rb
+++ fuego.rb
@@ -3,6 +3,13 @@ class Fuego < Formula
   url "http://svn.code.sf.net/p/fuego/code/trunk", :revision => 1981
   version "1.1.SVN"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-games"
+    sha1 "baada923a390e85d861e17f52ef5828a7b46507d" => :yosemite
+    sha1 "de717af5d96a517ece0f7567ce5c20f270a53790" => :mavericks
+    sha1 "cb3d147498cf47304dbffb7b3c68c66c9ac6b69c" => :mountain_lion
+  end
+
   head "http://svn.code.sf.net/p/fuego/code/trunk"
 
   depends_on "autoconf" => :build

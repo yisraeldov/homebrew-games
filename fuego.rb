9774c36e0b626342c1073b94e2794f8bb2e7069f
Jack Nagel <jacknagel@gmail.com>
2015-02-25 22:00:34 -0500

    fuego: fix autotools deps
    Closes #216.

--- fuego.rb
+++ fuego.rb
@@ -1,16 +1,19 @@
 class Fuego < Formula
   homepage "http://fuego.sourceforge.net/"
-  head "http://svn.code.sf.net/p/fuego/code/trunk"
   url "http://svn.code.sf.net/p/fuego/code/trunk", :revision => 1981
   version "1.1.SVN"
 
-  # automake should be a build dep, but won't install otherwise
-  depends_on 'automake'
-  depends_on 'boost'
+  head "http://svn.code.sf.net/p/fuego/code/trunk"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "boost"
 
   def install
-    system "autoreconf", "--force", "--install"
-    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
+    system "autoreconf", "-fvi"
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
     system "make", "install"
   end
 end

9643b354c2e6f58c8b620c9e744836763843bcb8
Pavel Puchkin <i@neoascetic.me>
2015-03-13 08:34:14 +0200

    minetest: support more compilation options
    Closes #225.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- minetest.rb
+++ minetest.rb
@@ -3,20 +3,17 @@ require "formula"
 class Minetest < Formula
   homepage "http://www.minetest.net/"
 
-  option "with-gettext", "Enable Gettext support"
-  option "with-freetype", "Use brewed version of Freetype"
-  option "without-client", "Build without client components"
-  option "without-server", "Build without server components"
-
   depends_on :x11
   depends_on "cmake" => :build
   depends_on "irrlicht"
   depends_on "jpeg"
   depends_on "libogg"
   depends_on "libvorbis"
-  depends_on "luajit"
-  depends_on "gettext" => :optional
-  depends_on "freetype" => :optional
+  depends_on "luajit" => :recommended
+  depends_on "freetype" => :recommended
+  depends_on "gettext" => :recommended
+  depends_on "leveldb" => :optional
+  depends_on "redis" => :optional
 
   head do
     url "https://github.com/minetest/minetest.git"
@@ -31,7 +28,8 @@ class Minetest < Formula
     sha1 "1ea404afd37ca3496b6ad8a8c6ebc28a317fe54c"
 
     resource "minetest_game" do
-      url "https://github.com/minetest/minetest_game.git", :tag => "0.4.12"
+      url "https://github.com/minetest/minetest_game/archive/0.4.12.tar.gz"
+      sha1 "f088e2b1072055ae65f089e2f8d1bb083569d63f"
     end
   end
 
@@ -47,11 +45,11 @@ class Minetest < Formula
     (buildpath/"games/minetest_game").install resource("minetest_game")
 
     args = std_cmake_args - %w{-DCMAKE_BUILD_TYPE=None}
-    args << '-DCMAKE_BUILD_TYPE=Release'
-    args << "-DENABLE_FREETYPE=1" if build.with? "freetype"
-    args << "-DBUILD_CLIENT=0" if build.without? "client"
-    args << "-DBUILD_SERVER=0" if build.without? "server"
-    args << "-DENABLE_GETTEXT=1" << "-DCUSTOM_GETTEXT_PATH=#{Formula["gettext"].opt_prefix}}" if build.with? "gettext"
+    args << "-DCMAKE_BUILD_TYPE=Release" << "-DBUILD_CLIENT=1" << "-DBUILD_SERVER=0"
+    args << "-DENABLE_REDIS=1" if build.with? "redis"
+    args << "-DENABLE_LEVELDB=1" if build.with? "leveldb"
+    args << "-DENABLE_FREETYPE=1" << "-DCMAKE_EXE_LINKER_FLAGS='-L#{Formula["freetype"].opt_lib}'" if build.with? "freetype"
+    args << "-DENABLE_GETTEXT=1" << "-DCUSTOM_GETTEXT_PATH=#{Formula["gettext"].opt_prefix}" if build.with? "gettext"
 
     # Their CMake cannot create simple app bundle now, but we may use "make
     # package" to create .dmg package with bundle inside and then copy bundle
@@ -63,5 +61,15 @@ class Minetest < Formula
 
     system "make", "package"
     prefix.install "_CPack_Packages/Bundle/minetest/minetest.app"
+  end
+
+  def caveats; <<-EOS.undent
+      Put additional subgames and mods into "games" and "mods" folders under
+      "~/Library/Application Support/minetest/", respectively (you may have
+      to create those folders first).
+
+      If you would like to start the Minetest server from a terminal, run
+      "/Applications/minetest.app/Contents/MacOS/minetest --server".
+    EOS
   end
 end

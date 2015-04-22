0590c2d67a33ed3ae3691516433cde4cce170315
Alex Dunn <dunn.alex@gmail.com>
2015-04-22 00:29:30 -0700

    freeciv 2.5.0
    Closes #240.
    Closes #228.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- freeciv.rb
+++ freeciv.rb
@@ -1,10 +1,16 @@
-require "formula"
-
 class Freeciv < Formula
   homepage "http://freeciv.wikia.com"
-  url "https://downloads.sourceforge.net/project/freeciv/Freeciv%202.4/2.4.4/freeciv-2.4.4.tar.bz2"
-  sha1 "045931a0763df33e1e2114dde2d189a24861ce13"
-  head "svn://svn.gna.org/svn/freeciv/trunk"
+  url "https://downloads.sourceforge.net/project/freeciv/Freeciv%202.5/2.5.0/freeciv-2.5.0.tar.bz2"
+  mirror "http://download.gna.org/freeciv/stable/freeciv-2.5.0.tar.bz2"
+  sha256 "bd9f7523ea79b8d2806d0c1844a9f48506ccd18276330580319913c43051210b"
+
+  head do
+    url "svn://svn.gna.org/svn/freeciv/trunk"
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+    depends_on "gettext" => :build
+    depends_on "libtool" => :build
+  end
 
   bottle do
     root_url "https://homebrew.bintray.com/bottles-games"
@@ -13,23 +19,34 @@ class Freeciv < Formula
     sha1 "a1a7dbcd4b0ee32c72586e14f7fe53fc5ec5797c" => :mountain_lion
   end
 
-  option "disable-nls" , "Disable NLS support"
-  option "without-sdl" , "Disable the SDL Freeciv client"
-  option "with-gtk+" , "Disable the GTK+ Freeciv client"
-  option "with-gtk+3" , "Enable the GTK+3 Freeciv client"
+  option "without-nls", "Disable NLS support"
+  option "without-sdl", "Disable the SDL Freeciv client"
+  option "with-gtk+", "Enable the GTK+ Freeciv client"
+  option "without-gtk+3", "Disable the GTK+3 Freeciv client"
 
+  depends_on "gettext" if build.with? "nls"
+  depends_on "icu4c"
   depends_on "pkg-config" => :build
   depends_on "readline"
   depends_on :x11
-  depends_on "gettext" unless build.include? "disable-nls"
 
   depends_on "sdl" => :recommended
-  depends_on "sdl_image" if build.with? "sdl"
-  depends_on "sdl_mixer" if build.with? "sdl"
+  if build.with? "sdl"
+    depends_on "freetype"
+    depends_on "sdl_image"
+    depends_on "sdl_gfx"
+    depends_on "sdl_mixer"
+    depends_on "sdl_ttf"
+  end
 
   depends_on "gtk+" => :optional
-  depends_on "gtk+3" => :optional
-  depends_on "glib" if build.with?("gtk+") || build.with?("gtk+3")
+  depends_on "gtk+3" => :recommended
+  if build.with?("gtk+") || build.with?("gtk+3")
+    depends_on "atk"
+    depends_on "glib"
+    depends_on "pango"
+  end
+  depends_on "gdk-pixbuf" if build.with? "gdk+3"
 
   def install
     args = %W[
@@ -39,7 +56,7 @@ class Freeciv < Formula
       --with-readline=#{Formula["readline"].opt_prefix}
     ]
 
-    if build.include? "disable-nls"
+    if build.without? "nls"
       args << "--disable-nls"
     else
       gettext = Formula["gettext"]
@@ -47,16 +64,25 @@ class Freeciv < Formula
       args << "LDFLAGS=-L#{gettext.lib}"
     end
 
-    build_client = []
-    build_client << "sdl" if build.with? "sdl"
-    build_client << "gtk" if build.with? "gtk+"
-    build_client << "gtk+3" if build.with? "gtk+3"
-    args << "--enable-client=" + build_client.join(",") unless build_client.empty?
-    system "./configure", *args
+    if build.head?
+      inreplace "./autogen.sh", "libtoolize", "glibtoolize"
+      system "./autogen.sh", *args
+    else
+      system "./configure", *args
+    end
+
     system "make", "install"
   end
 
   test do
-    system "#{bin}/freeciv-server", "-v"
+    system bin/"freeciv-manual"
+    File.exist? testpath/"manual6.html"
+
+    server = fork do
+      system bin/"freeciv-server", "-l", testpath/"test.log"
+    end
+    sleep 5
+    Process.kill("TERM", server)
+    File.exist? testpath/"test.log"
   end
 end

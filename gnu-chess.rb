67997a2376abb021c9163b6647820c34e06bb6fb
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-23 19:49:24 +0100

    gnu-chess: add 6.2.1 bottle.

--- gnu-chess.rb
+++ gnu-chess.rb
@@ -4,6 +4,13 @@ class GnuChess < Formula
   mirror "https://ftp.gnu.org/gnu/chess/gnuchess-6.2.1.tar.gz"
   sha256 "17caab725539447bcb17a14b17905242cbf287087e53a6777524feb7bbaeed06"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-games"
+    sha256 "6f5fa679cc037398f4b1ddc4d7d1f442389e21c9745d34c0eae7b3b9710f9b40" => :yosemite
+    sha256 "ae572a58d0c7d2c298e373b05d86f0bc6d4a932b77ee1b08a4846ccaa7df6aab" => :mavericks
+    sha256 "ff097e03ea2d16d8a677504ff065a801bbcf47a335c24a7a7ee1255c8d86f47d" => :mountain_lion
+  end
+
   option "with-book", "Download the opening book (~25MB)"
 
   resource "book" do

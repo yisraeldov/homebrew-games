aeb3a8532aea9121ca07eaf29e9e9012d19bfadc
BrewTestBot <brew-test-bot@googlegroups.com>
2015-04-25 04:22:59 +0100

    stockfish: add 6 bottle.

--- stockfish.rb
+++ stockfish.rb
@@ -6,6 +6,14 @@ class Stockfish < Formula
   sha1 "a2d630c991d13bc5533c72896421eff14e4faa6a"
   head "https://github.com/official-stockfish/Stockfish.git"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-games"
+    cellar :any
+    sha256 "622bacb36395cb7ccdb83510c354292d954aa655201fdc506185de99b18de75d" => :yosemite
+    sha256 "f02a0eaa017869d57cdb713ab8e20c70122ed17fa60f8add1f2db1933b843ec2" => :mavericks
+    sha256 "6dda7191589eb57f3145681eeb6868dc528d0a3ccd217213177378c57f6a3873" => :mountain_lion
+  end
+
   def install
     if Hardware::CPU.features.include? :popcnt
       arch = "x86-64-modern"

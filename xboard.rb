88f6a8bb1ca2bde92fd1ad83deca24ea0ab75a4c
Larry Gilbert <larry@l2g.to>
2015-05-01 03:28:24 -0700

    xboard: brew audit --strict
    Closes #249.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- xboard.rb
+++ xboard.rb
@@ -1,14 +1,14 @@
 class Xboard < Formula
-  homepage 'https://www.gnu.org/software/xboard/'
-  url 'http://ftpmirror.gnu.org/xboard/xboard-4.7.3.tar.gz'
-  mirror 'https://ftp.gnu.org/gnu/xboard/xboard-4.7.3.tar.gz'
-  sha1 'dab9a69f3c8afd70a7a2e5e31f48d31fd9fbbabb'
+  homepage "https://www.gnu.org/software/xboard/"
+  url "http://ftpmirror.gnu.org/xboard/xboard-4.7.3.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/xboard/xboard-4.7.3.tar.gz"
+  sha256 "7fd0b03f53dad57c587bc3438459612e2455534f715cfb0e637b6290f34cbeaa"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'fairymax' => :recommended
-  depends_on 'gettext'
-  depends_on 'cairo'
-  depends_on 'librsvg'
+  depends_on "pkg-config" => :build
+  depends_on "fairymax" => :recommended
+  depends_on "gettext"
+  depends_on "cairo"
+  depends_on "librsvg"
   depends_on :x11
 
   def install
@@ -19,6 +19,10 @@ class Xboard < Formula
 
     system "./configure", *args
     system "make"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system bin/"xboard", "--help"
   end
 end

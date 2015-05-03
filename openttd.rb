2970d3933d1244f838a40cabc7fbeae7eb240415
Alex Dunn <dunn.alex@gmail.com>
2015-05-03 16:43:08 -0700

    openttd 1.5.0
    Closes #253.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- openttd.rb
+++ openttd.rb
@@ -1,47 +1,45 @@
-require 'formula'
-
 class Openttd < Formula
-  homepage 'http://www.openttd.org/'
-  url 'http://binaries.openttd.org/releases/1.4.4/openttd-1.4.4-source.tar.gz'
-  sha1 'baa63da481eb59bff494354c9ebae60844409ad7'
+  homepage "http://www.openttd.org/"
+  url "http://binaries.openttd.org/releases/1.5.0/openttd-1.5.0-source.tar.xz"
+  sha256 "cb2735c3c94709430c58eb4e8820cd5d26b1a17447c34ca8792bb3432a3f7c2d"
 
-  head 'git://git.openttd.org/openttd/trunk.git'
+  head "git://git.openttd.org/openttd/trunk.git"
 
-  depends_on 'lzo'
-  depends_on 'xz'
-  depends_on 'pkg-config' => :build
+  depends_on "lzo"
+  depends_on "xz"
+  depends_on "pkg-config" => :build
 
-  resource 'opengfx' do
-    url 'http://bundles.openttdcoop.org/opengfx/releases/0.5.1/opengfx-0.5.1.zip'
-    sha1 'f2700bc86ae49d2cdadc5b56d8abb7d49b4f2167'
+  resource "opengfx" do
+    url "http://bundles.openttdcoop.org/opengfx/releases/0.5.2/opengfx-0.5.2.zip"
+    sha256 "19be61f1cb04cbb3cb9602f0b8eb6e6f56ecbefbfdd6e0e03f9579e5a5c1cbc8"
   end
 
-  resource 'opensfx' do
-    url 'http://bundles.openttdcoop.org/opensfx/releases/0.2.3/opensfx-0.2.3.zip'
-    sha1 'bfbfeddb91ff32a58a68488382636f38125c48f4'
+  resource "opensfx" do
+    url "http://bundles.openttdcoop.org/opensfx/releases/0.2.3/opensfx-0.2.3.zip"
+    sha256 "3574745ac0c138bae53b56972591db8d778ad9faffd51deae37a48a563e71662"
   end
 
-  resource 'openmsx' do
-    url 'http://bundles.openttdcoop.org/openmsx/releases/0.3.1/openmsx-0.3.1.zip'
-    sha1 'e9c4203923bb9c974ac67886bd00b7090658b961'
+  resource "openmsx" do
+    url "http://bundles.openttdcoop.org/openmsx/releases/0.3.1/openmsx-0.3.1.zip"
+    sha256 "92e293ae89f13ad679f43185e83fb81fb8cad47fe63f4af3d3d9f955130460f5"
   end
 
-  # Ensures a deployment target is not set on 10.9;
-  # TODO report this upstream
+  # Ensures a deployment target is not set on 10.9:
+  # https://bugs.openttd.org/task/6295
   patch :p0 do
     url "https://trac.macports.org/export/117147/trunk/dports/games/openttd/files/patch-config.lib-remove-deployment-target.diff"
-    sha1 "4093cccc269fdd1306b33ae2ec733865491b14c8"
+    sha256 "95c3d54a109c93dc88a693ab3bcc031ced5d936993f3447b875baa50d4e87dac"
   end
 
   def install
     system "./configure", "--prefix-dir=#{prefix}"
-    system "make bundle"
+    system "make", "bundle"
 
-    (buildpath/'bundle/OpenTTD.app/Contents/Resources/data/opengfx').install resource('opengfx')
-    (buildpath/'bundle/OpenTTD.app/Contents/Resources/data/opensfx').install resource('opensfx')
-    (buildpath/'bundle/OpenTTD.app/Contents/Resources/gm/openmsx').install resource('openmsx')
+    (buildpath/"bundle/OpenTTD.app/Contents/Resources/data/opengfx").install resource("opengfx")
+    (buildpath/"bundle/OpenTTD.app/Contents/Resources/data/opensfx").install resource("opensfx")
+    (buildpath/"bundle/OpenTTD.app/Contents/Resources/gm/openmsx").install resource("openmsx")
 
-    prefix.install 'bundle/OpenTTD.app'
+    prefix.install "bundle/OpenTTD.app"
   end
 
   def caveats; <<-EOS.undent
@@ -50,5 +48,9 @@ class Openttd < Formula
       Transport Tycoon Deluxe, you can install them by following the
       instructions in section 4.1 of #{prefix}/readme.txt
     EOS
+  end
+
+  test do
+    File.executable? prefix/"OpenTTD.app/Contents/MacOS/openttd"
   end
 end

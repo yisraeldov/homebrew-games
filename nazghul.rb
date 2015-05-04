6e497fcd199e2b1b0d5f67feb31f34ef34ee6003
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-04 01:39:47 +0100

    nazghul: add 0.7.1 bottle.

--- nazghul.rb
+++ nazghul.rb
@@ -3,6 +3,14 @@ class Nazghul < Formula
   url "https://downloads.sourceforge.net/project/nazghul/nazghul/nazghul-0.7.1/nazghul-0.7.1.tar.gz"
   sha256 "f1b62810da52a116dfc1c407dbe683991b1b380ca611f57b5701cfbb803e9d2b"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-games"
+    cellar :any
+    sha256 "463268eb22b46f6197ccfc8c7042306cc63d9da14fb64e39af391fa8e63b4bcd" => :yosemite
+    sha256 "be226e182e99ef8944baf85c34c46f3820e24ccb3052d1f267d2edc6d313f02c" => :mavericks
+    sha256 "8212dbd986171a00d9f2a986ef52394daf2cfc98ed20b8697332880cd20f6343" => :mountain_lion
+  end
+
   depends_on "sdl"
   depends_on "sdl_image"
   depends_on "sdl_mixer"

d28653defb87c8278911bc76677b75c1c083a4f5
Xu Cheng <xucheng@me.com>
2015-05-10 17:28:54 +0800

    ckan 1.6.14

--- ckan.rb
+++ ckan.rb
@@ -6,17 +6,14 @@ end
 
 class Ckan < Formula
   homepage "https://github.com/KSP-CKAN/CKAN/"
-  # CKAN releases a raw .exe file
-  url "https://github.com/KSP-CKAN/CKAN/releases/download/v1.6.11/ckan.exe", :using => :nounzip
-  version "1.6.11"
-  sha256 "95ea198e702c145ab21ad30e7e4e73a40bae4d86d87c5e03ef150cbf7b505f23"
+  url "https://github.com/KSP-CKAN/CKAN/releases/download/v1.6.14/ckan.exe", :using => :nounzip
+  version "1.6.14"
+  sha256 "21619df57bdff0aa030109ab7d8d94309a4041e8bbd689f111550eb41151d63f"
 
   depends_on MonoDependency
 
   def install
     (libexec/"bin").install "ckan.exe"
-    # The usual method for running ckan.exe is `mono ckan.exe arg1 arg2 ...`
-    # This is a wrapper script for aliasing it as `ckan arg1 arg2 ...` into the PATH
     (bin/"ckan").write <<-EOS.undent
       #!/bin/sh
       exec mono "#{libexec}/bin/ckan.exe" "$@"

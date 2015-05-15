2c3d94d121239204a089ea5eab80b402b61e3347
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-15 03:01:51 +0100

    stormlib: add 9.10 bottle.

--- stormlib.rb
+++ stormlib.rb
@@ -5,6 +5,14 @@ class Stormlib < Formula
 
   head "https://github.com/ladislav-zezula/StormLib.git"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-games"
+    cellar :any
+    sha256 "21dc183f56bd9652902191f6926a2742941434d8857c6647a4e1f0ddfef83a0a" => :yosemite
+    sha256 "c3c8c3ac7ac27f8ab04a33d56cabc8f2c6b30f30d23099c0fd45b1068e952021" => :mavericks
+    sha256 "e9c78128750bf0982d4296a94c396a6f16af923a2754d619f9e9229159e92a8f" => :mountain_lion
+  end
+
   depends_on "cmake" => :build
 
   # prevents cmake from trying to write to /Library/Frameworks/

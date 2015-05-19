e3f81ab65069e3883c3d3ecf25092309486ac161
Alex Dunn <dunn.alex@gmail.com>
2015-05-18 17:33:45 -0700

    ski 6.9
    Closes #257.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- ski.rb
+++ ski.rb
@@ -1,11 +1,18 @@
-require 'formula'
-
 class Ski < Formula
-  url 'http://www.catb.org/~esr/ski/ski-6.8.tar.gz'
-  homepage 'http://catb.org/~esr/ski/'
-  sha1 '500c3ebff28dfe2b7a4c9c4c4690c0dea7a3e32a'
+  url "http://www.catb.org/~esr/ski/ski-6.9.tar.gz"
+  homepage "http://catb.org/~esr/ski/"
+  sha256 "d0f2fd7770a63319340fab8ee275c6221dff0e0e7fe488f2298f567583902d37"
+
+  head do
+    url "git://thyrsus.com/repositories/ski.git"
+    depends_on "xmlto" => :build
+  end
 
   def install
+    if build.head?
+      ENV["XML_CATALOG_FILES"] = "#{etc}/xml/catalog"
+      system "make"
+    end
     bin.install "ski"
     man6.install "ski.6"
   end

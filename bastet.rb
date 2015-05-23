dac1f4d8af5c0a0946b0b49c92af8fe7e5f8bacd
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-23 08:53:12 +0100

    bastet: add 0.43.1 bottle.

--- bastet.rb
+++ bastet.rb
@@ -3,6 +3,14 @@ class Bastet < Formula
   url "https://github.com/fph/bastet/archive/0.43.1.tar.gz"
   sha256 "c47a84fb17c2895ea7a85b72ea40a154a03c1114c178ea7fee341215153afcdc"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-games"
+    cellar :any
+    sha256 "2b8b98b85c99146690f363dc780d946c8fe8cecfb838ab3fb0dbad8614b83bc5" => :yosemite
+    sha256 "c246613779b82dac07649d5d53b846f38ffababa8d7ec40e405e0cc91a046686" => :mavericks
+    sha256 "cf39c88f44e2786e109e3d87bc2da1e954ae70245d452a938e66115f77576e68" => :mountain_lion
+  end
+
   # "friend declaration specifying a default argument must be a definition"
   patch do
     url "https://github.com/fph/bastet/commit/0323cb477dd5293b5198e4b2f47b4441d90de2d8.patch"

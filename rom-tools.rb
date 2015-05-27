ffa065c8c6e2cb0bcd1b6cbf08547ec717273d2b
Parry <mbcoguno@users.noreply.github.com>
2015-05-27 23:32:42 +0800

    rom-tools 0.162
    Add missing tools.
    
    Closes #262.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- rom-tools.rb
+++ rom-tools.rb
@@ -1,8 +1,8 @@
 class RomTools < Formula
   homepage "http://mamedev.org/"
-  url "https://github.com/mamedev/mame/archive/mame0161.tar.gz"
-  sha256 "f7db934676e90d0d7f2b678ccf32e580417c754dd33117ec683560956c2130b9"
-  version "0.161"
+  url "https://github.com/mamedev/mame/archive/mame0162.tar.gz"
+  sha256 "97b5154177cf7fa862f72be0a5a1241d4560a44bc00c2ac1785f0ae706deb899"
+  version "0.162"
 
   head "https://github.com/mamedev/mame.git"
 
@@ -16,13 +16,6 @@ class RomTools < Formula
 
   depends_on "sdl2"
 
-  # Use OpenGL extensions for non-framework SDL 2 environment.
-  # Upstream: <https://github.com/mamedev/mame/pull/170>
-  patch do
-    url "https://github.com/mbcoguno/mame/commit/05f71a27f9.diff"
-    sha256 "d7c82d97cc9367226b44bcd174b98ce56f96b034f12a0af7cae1da76065a947c"
-  end
-
   def install
     ENV["MACOSX_USE_LIBSDL"] = "1"
     ENV["PTR64"] = (MacOS.prefer_64_bit? ? "1" : "0")
@@ -30,8 +23,8 @@ class RomTools < Formula
     system "make", "CC=#{ENV.cc}", "LD=#{ENV.cxx}", "TARGET=ldplayer", "TOOLS=1"
 
     bin.install %W[
-      chdman jedutil ldresample ldverify nltool pngcmp regrep romcmp src2html
-      srcclean testkeys unidasm
+      castool chdman floptool imgtool jedutil ldresample ldverify nltool pngcmp
+      regrep romcmp src2html srcclean testkeys unidasm
     ]
     bin.install "split" => "rom-split"
     if MacOS.prefer_64_bit?
@@ -39,8 +32,6 @@ class RomTools < Formula
     else
       bin.install "ldplayer"
     end
-    %W[chdman jedutil ldplayer ldresample ldverify romcmp testkeys].each do |manfile|
-      man1.install "src/osd/sdl/man/#{manfile}.1"
-    end
+    man1.install Dir["src/osd/sdl/man/*.1"]
   end
 end

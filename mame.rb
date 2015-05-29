5e7e4aa79e759594ff7b39e57a92996216d2db1f
Parry <mbcoguno@users.noreply.github.com>
2015-05-29 09:36:09 +0800

    mame 0.162
    Closes #263.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- mame.rb
+++ mame.rb
@@ -1,8 +1,8 @@
 class Mame < Formula
   homepage "http://mamedev.org/"
-  url "https://github.com/mamedev/mame/archive/mame0161.tar.gz"
-  sha256 "f7db934676e90d0d7f2b678ccf32e580417c754dd33117ec683560956c2130b9"
-  version "0.161"
+  url "https://github.com/mamedev/mame/archive/mame0162.tar.gz"
+  sha256 "97b5154177cf7fa862f72be0a5a1241d4560a44bc00c2ac1785f0ae706deb899"
+  version "0.162"
 
   head "https://github.com/mamedev/mame.git"
 
@@ -15,13 +15,6 @@ class Mame < Formula
   end
 
   depends_on "sdl2"
-
-  # Use OpenGL extensions for non-framework SDL 2 environment.
-  # Upstream: <https://github.com/mamedev/mame/pull/170>
-  patch do
-    url "https://github.com/mbcoguno/mame/commit/05f71a27f9.diff"
-    sha256 "d7c82d97cc9367226b44bcd174b98ce56f96b034f12a0af7cae1da76065a947c"
-  end
 
   def install
     ENV["MACOSX_USE_LIBSDL"] = "1"

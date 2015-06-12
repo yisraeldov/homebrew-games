ea74d5e3f2b50a112b6b8d47c6e442cb31596bfa
Martin Plicht <mardmol@gmx.de>
2015-06-12 19:32:37 +0200

    openclonk 6.1
    Closes #267.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- openclonk.rb
+++ openclonk.rb
@@ -1,8 +1,8 @@
 class Openclonk < Formula
-  homepage "http://www.openclonk.org"
-  url "https://github.com/openclonk/openclonk/archive/v6.0.tar.gz"
-  sha256 "1daa437a598f09375d85d0cd6cfb8655f285ae9f1939221cea2f587e531133ba"
   head "https://github.com/openclonk/openclonk", :using => :git
+  homepage "http://www.openclonk.org"
+  url "https://github.com/openclonk/openclonk/archive/v6.1.tar.gz"
+  sha256 "4e2e6cefedd4a13523593b285af23b530caa3a98ff02ac4adb215cf32889cb94"
 
   bottle do
     root_url "https://homebrew.bintray.com/bottles-games"
@@ -24,16 +24,6 @@ class Openclonk < Formula
   depends_on "freealut"
 
   needs :cxx11
-
-  patch do
-    url "https://github.com/openclonk/openclonk/commit/5c9d687d67a5c159a07dabb385b236996f6de668.diff"
-    sha256 "93c2698bf20cf8cf6c11cf6a128dd4841435efa47ca4ced06c2ace0e60f63481"
-  end
-
-  patch do
-    url "https://github.com/openclonk/openclonk/commit/c82adb78537515c02368678e39942c4df61d7bd5.diff"
-    sha256 "5736fb918002cb3a5cc096d00656664aa5ba944c7ca60a963bc8c6597328384b"
-  end
 
   def install
     ENV.cxx11

0c2dc3ef80c7512a1eade2e9848c8f4dac3f46c9
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-19 09:15:55 +0100

    ckan: add 1.6.23 bottle.

--- ckan.rb
+++ ckan.rb
@@ -10,6 +10,14 @@ class Ckan < Formula
   version "1.6.23"
   sha256 "6707f650b60d95f2a077012f0bcfffb8fef47b7607848d400b19d915907b143f"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-games"
+    cellar :any
+    sha256 "d5448287d4c3fae3c67e528067dd81465e102c483e05e11fc95dd51ee447efc4" => :yosemite
+    sha256 "071d9e8b87007e9c447c649c8e2554ef79eeb899792e6d5d48308bd626c166b9" => :mavericks
+    sha256 "6d30325a2245db52cfab2020f41f501537651d6d9d2a8dcd5a43d3a226c24be5" => :mountain_lion
+  end
+
   depends_on MonoDependency
 
   def install

e62740a8e40f65e9127ff5c01294d0d1187e548b
Kyungdahm Yun <tomyun@gmail.com>
2015-06-25 00:44:21 -0700

    rom-tools 0.163
    Closes #277.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- rom-tools.rb
+++ rom-tools.rb
@@ -1,10 +1,19 @@
 class RomTools < Formula
+  desc "Tools for Multiple Arcade Machine Emulator"
   homepage "http://mamedev.org/"
-  url "https://github.com/mamedev/mame/archive/mame0162.tar.gz"
-  sha256 "97b5154177cf7fa862f72be0a5a1241d4560a44bc00c2ac1785f0ae706deb899"
-  version "0.162"
+  url "https://github.com/mamedev/mame/archive/mame0163.tar.gz"
+  version "0.163"
+  sha256 "6be1536f0c8470764aead9a32f8b5d9fd7979040b402ec337cb47f73deaa68b1"
 
   head "https://github.com/mamedev/mame.git"
+
+  # FIXME: only for clang build error on 0.163
+  stable do
+    patch do
+      url "https://github.com/mamedev/mame/commit/bc23c001b5dff4b271fc480c55d56785e9e3fa10.diff"
+      sha256 "0541c65a7354125d59303ada00b13f9754af4acf0844ac1145c3cb648cfb6cf7"
+    end
+  end
 
   bottle do
     root_url "https://homebrew.bintray.com/bottles-games"
@@ -33,5 +42,9 @@ class RomTools < Formula
       bin.install "ldplayer"
     end
     man1.install Dir["src/osd/sdl/man/*.1"]
+  end
+
+  test do
+    system "#{bin}/ldplayer", "-help"
   end
 end

abdd01ae804d188f6bc246b04eeecff9f6632694
Misty De Meo <mistydemeo@gmail.com>
2015-07-15 21:12:40 -0700

    frotz: use HTTPS Github URL

--- frotz.rb
+++ frotz.rb
@@ -4,7 +4,8 @@ class Frotz < Formula
   url 'https://github.com/DavidGriffith/frotz/archive/2.44.tar.gz'
   homepage 'https://github.com/DavidGriffith/frotz'
   sha256 'dbb5eb3bc95275dcb984c4bdbaea58bc1f1b085b20092ce6e86d9f0bf3ba858f'
-  head 'git@github.com:DavidGriffith/frotz.git'
+
+  head 'https://github.com/DavidGriffith/frotz.git'
 
   bottle do
     cellar :any

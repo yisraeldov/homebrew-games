d4863b475c32096bfaa0bb7085561bada6601c58
Vladislav Rassokhin <vladrassokhin@gmail.com>
2015-07-17 22:07:53 +0300

    ckan 1.10.3
    Closes #282.
    
    Signed-off-by: Xu Cheng <xucheng@me.com>

--- ckan.rb
+++ ckan.rb
@@ -6,9 +6,9 @@ end
 
 class Ckan < Formula
   homepage "https://github.com/KSP-CKAN/CKAN/"
-  url "https://github.com/KSP-CKAN/CKAN/releases/download/v1.8.3/ckan.exe", :using => :nounzip
-  version "1.8.3"
-  sha256 "70f58530a97d980a04ca88757e0f0d3acef7944dfd72e5f5c5de2de0ee8e33f9"
+  url "https://github.com/KSP-CKAN/CKAN/releases/download/v1.10.3/ckan.exe", :using => :nounzip
+  version "1.10.3"
+  sha256 "9771e55ee4463dae33274a233997e0f8aa5a564e0f2fa7a3d092230389c66be1"
 
   depends_on MonoDependency
 

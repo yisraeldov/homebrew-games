e1b8d5736452ebb0f023049e94effd7dae6a7323
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-13 07:44:06 +0100

    omega-rpg: add 0.80.2 bottle.

--- omega-rpg.rb
+++ omega-rpg.rb
@@ -4,6 +4,12 @@ class OmegaRpg < Formula
   url "http://www.alcyone.com/binaries/omega/omega-0.80.2-src.tar.gz"
   sha256 "60164319de90b8b5cae14f2133a080d5273e5de3d11c39df080a22bbb2886104"
 
+  bottle do
+    sha256 "1c0b199b608f8f3e4544243743049ffd334d26680cc372adf1c9c4aeb0420f55" => :yosemite
+    sha256 "25bef4995836f8d1e2f66a1daafe0220a050c419e30ebf6dffbdfd567fc2ac5c" => :mavericks
+    sha256 "5f492f394cbef8f1674cb5aa25a30e946a42288ba40e16958aae1789110b5568" => :mountain_lion
+  end
+
   def install
     # Set up our target folders
     inreplace "defs.h", "#define OMEGALIB \"./omegalib/\"", "#define OMEGALIB \"#{libexec}/\""

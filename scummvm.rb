841eb05ab35cad161a6e97f6dfef41f5212d0dff
Kyungdahm Yun <tomyun@gmail.com>
2015-08-18 01:28:08 -0700

    scummvm: add more deps / options
    Closes #302.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- scummvm.rb
+++ scummvm.rb
@@ -1,22 +1,37 @@
-require 'formula'
-
 class Scummvm < Formula
-  url 'https://downloads.sourceforge.net/project/scummvm/scummvm/1.7.0/scummvm-1.7.0.tar.bz2'
-  homepage 'http://www.scummvm.org/'
-  sha1 'a16c84f457b6cdb405739eab72c2106f4674bc75'
-  head 'https://github.com/scummvm/scummvm.git'
+  desc "Graphic adventure game interpreter"
+  homepage "http://scummvm.org/"
+  url "https://downloads.sourceforge.net/project/scummvm/scummvm/1.7.0/scummvm-1.7.0.tar.bz2"
+  sha256 "d9ff0e8cf911afa466d5456d28fef692a17d47ddecfd428bf2fef591237c2e66"
+  head "https://github.com/scummvm/scummvm.git"
 
-  depends_on 'sdl'
-  depends_on "libpng"
-  depends_on 'flac' => :recommended
-  depends_on 'libvorbis' => :recommended
-  depends_on 'libogg' => :recommended
-  depends_on 'fluid-synth' => :optional
+  option "with-all-engines", "Enable all engines (including broken or unsupported)"
+
+  depends_on "sdl"
+  depends_on "libvorbis" => :recommended
+  depends_on "mad" => :recommended
+  depends_on "flac" => :recommended
+  depends_on "libmpeg2" => :optional
+  depends_on "jpeg" => :recommended
+  depends_on "libpng" => :recommended
+  depends_on "theora" => :recommended
+  depends_on "faad2" => :recommended
+  depends_on "fluid-synth" => :recommended
+  depends_on "freetype" => :recommended
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-debug"
+    args = %W[
+      --prefix=#{prefix}
+      --enable-release
+    ]
+    args << "--enable-all-engines" if build.with? "all-engines"
+    system "./configure", *args
     system "make"
-    system "make install"
-    (share+'pixmaps').rmtree
+    system "make", "install"
+    (share+"pixmaps").rmtree
+  end
+
+  test do
+    system "#{bin}/scummvm", "-v"
   end
 end

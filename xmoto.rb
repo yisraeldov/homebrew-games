f1db675d06dfc6eefedf1e407244b0a398a6e9ee
Kyungdahm Yun <tomyun@gmail.com>
2015-08-19 02:02:26 -0700

    xmoto 0.5.11
    Closes #306.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- xmoto.rb
+++ xmoto.rb
@@ -1,43 +1,58 @@
-require 'formula'
-
 class Xmoto < Formula
-  url 'http://download.tuxfamily.org/xmoto/xmoto/0.5.10/xmoto-0.5.10-src.tar.gz'
-  homepage 'http://xmoto.tuxfamily.org/'
-  sha1 '692d50a9c91791cd06ee84846632651b44544fcc'
+  desc "2D motocross platform game"
+  homepage "http://xmoto.tuxfamily.org/"
+  url "http://download.tuxfamily.org/xmoto/xmoto/0.5.11/xmoto-0.5.11-src.tar.gz"
+  sha256 "a584a6f9292b184686b72c78f16de4b82d5c5b72ad89e41912ff50d03eca26b2"
 
-  depends_on 'libxml2'
-  depends_on 'gettext'
-  depends_on 'jpeg'
-  depends_on 'sdl'
-  depends_on 'sdl_mixer'
-  depends_on 'sdl_net'
-  depends_on 'sdl_ttf'
-  depends_on 'lua'
-  depends_on 'ode'
+  head do
+    url "svn://svn.tuxfamily.org/svnroot/xmoto/xmoto/trunk"
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+  end
+
+  depends_on "sdl"
+  depends_on "sdl_mixer"
+  depends_on "sdl_net"
+  depends_on "sdl_ttf"
+  depends_on "ode"
   depends_on "libpng"
-
-  # Fixes compiling against libpng on Lion. See:
-  # http://todo.xmoto.tuxfamily.org/index.php?do=details&task_id=783
-  patch :DATA
+  depends_on "jpeg"
+  depends_on "libxml2"
+  depends_on "gettext" => :recommended
+  depends_on "libxdg-basedir"
+  depends_on "lua" => :recommended
 
   def install
+    # Fix issues reported upstream
+    # http://todo.xmoto.tuxfamily.org/index.php?do=details&task_id=812
+
+    # Set up single precision ODE
+    ENV.append_to_cflags "-DdSINGLE"
+
+    # Use same type as Apple OpenGL.framework
+    inreplace "src/glext.h", "unsigned int GLhandleARB", "void *GLhandleARB"
+
+    # Handle quirks of C++ hash_map
+    inreplace "src/include/xm_hashmap.h" do |s|
+      if build.head?
+        s.gsub! "tr1/", ""
+        s.gsub! "::tr1", ""
+      else
+        s.gsub! "s2) {", "s2) const {"
+      end
+    end
+
+    system "./bootstrap" if build.head?
     system "./configure", "--prefix=#{prefix}",
+                          "--disable-dependency-tracking",
+                          "--disable-xmltest",
                           "--disable-sdltest",
-                          "--with-internal-xdg=1",
-                          "--with-apple-opengl-framework"
-    system "make install"
+                          "--with-apple-opengl-framework",
+                          "--with-asian-ttf-file="
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/xmoto", "-h"
   end
 end
-
-__END__
-diff --git a/src/image/tim_png.cpp b/src/image/tim_png.cpp
-index 75fd545..a1d1cf6 100644
---- a/src/image/tim_png.cpp
-+++ b/src/image/tim_png.cpp
-@@ -24,6 +24,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-  */
- #include <stdio.h>
- #include <string.h>
-+#include <zlib.h>
- #include "tim.h"
- #include "png.h"

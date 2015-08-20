020c27440d242c711e859fb25668f92fa8c12fd0
Xu Cheng <xucheng@me.com>
2015-08-20 18:24:30 +0800

    ckan 1.12.0

--- ckan.rb
+++ ckan.rb
@@ -1,16 +1,17 @@
-class MonoDependency < Requirement
+class MonoRequirement < Requirement
   fatal true
   default_formula "mono"
   satisfy { which("mono") }
 end
 
 class Ckan < Formula
+  desc "The Comprehensive Kerbal Archive Network"
   homepage "https://github.com/KSP-CKAN/CKAN/"
-  url "https://github.com/KSP-CKAN/CKAN/releases/download/v1.10.3/ckan.exe", :using => :nounzip
-  version "1.10.3"
-  sha256 "9771e55ee4463dae33274a233997e0f8aa5a564e0f2fa7a3d092230389c66be1"
+  url "https://github.com/KSP-CKAN/CKAN/releases/download/v1.12.0/ckan.exe", :using => :nounzip
+  version "1.12.0"
+  sha256 "7056e1a9f02f25e8592562f9d128a0de02d6bd40e9c0defd8cf165dfec0083cc"
 
-  depends_on MonoDependency
+  depends_on MonoRequirement
 
   def install
     (libexec/"bin").install "ckan.exe"

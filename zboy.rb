978260e1e8820628782709d6ea29a891600f01d9
Kyungdahm Yun <tomyun@gmail.com>
2015-08-22 05:01:32 -0700

    zboy 0.60 (new formula)
    Closes #316.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

---
+++ zboy.rb
@@ -0,0 +1,20 @@
+class Zboy < Formula
+  desc "GameBoy emulator"
+  homepage "http://zboy.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/zboy/zBoy%20v0.60/zboy-0.60.tar.gz"
+  sha256 "f81e61433a5b74c61ab84cac33da598deb03e49699f3d65dcb983151a6f1c749"
+  head "http://svn.code.sf.net/p/zboy/code/trunk"
+
+  depends_on "sdl2"
+
+  def install
+    sdl2 = Formula["sdl2"]
+    ENV.append_to_cflags "-std=gnu89 -D__zboy4linux__ -DNETPLAY -DLFNAVAIL -I#{sdl2.include} -L#{sdl2.lib}"
+    system "make", "-f", "Makefile.linux", "CFLAGS=#{ENV.cflags}"
+    bin.install "zboy"
+  end
+
+  test do
+    system "zboy", "--help"
+  end
+end

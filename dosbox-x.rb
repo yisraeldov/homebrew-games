7d0b552b7159a6e371a100c98eacce67fa77a12e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-23 07:37:27 +0100

    dosbox-x: add 0.801 bottle.

--- dosbox-x.rb
+++ dosbox-x.rb
@@ -7,6 +7,13 @@ class DosboxX < Formula
     url "https://github.com/joncampbell123/dosbox-x/archive/v0.801.tar.gz"
     sha256 "40f94cdcc5c9a374c522de7eb2c2288eaa8c6de85d0bd6a730f48bd5d84a89f9"
   end
+  bottle do
+    cellar :any
+    sha256 "dd180aec251c141215aad2d202f1338025d4898e301e8fe0cbc6ba412f12652c" => :yosemite
+    sha256 "ef7faa65a136f1385220697e1a9dc5ce3c29fb09c54d55200921c83ea8094979" => :mavericks
+    sha256 "cbe3b3c41f82ecb32a3abbb23b6df05b7916c9fbe7413e7de680bb1c5a8502f7" => :mountain_lion
+  end
+
 
   depends_on "sdl"
   depends_on "sdl_net"

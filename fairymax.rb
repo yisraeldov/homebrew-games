578e94d64bd8c1bb008116cd862672e043ff98a5
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-23 04:08:58 +0100

    fairymax: add 4.8V bottle.

--- fairymax.rb
+++ fairymax.rb
@@ -5,6 +5,12 @@ class Fairymax < Formula
   version "4.8V"
   head "http://hgm.nubati.net/git/fairymax.git"
 
+  bottle do
+    sha256 "452353f678194a579cf910183f0a147ebfdfe30fc48bd29f7d0499f53c25fdca" => :yosemite
+    sha256 "8d18b3bb91fd8d4158e7f074a0585256842a6c4d86a2ae8911b35271d86f2218" => :mavericks
+    sha256 "7fa32e1ece7c7b0d3b8b18765a7377eaae1218fff9521d1e523cbe275ecc6435" => :mountain_lion
+  end
+
   def install
     system "make", "all",
                    "INI_F=#{pkgshare}/fmax.ini",

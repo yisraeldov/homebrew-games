56aa9bcb07df671b11e83f56a94c18df2bb148c7
BrewTestBot <brew-test-bot@googlegroups.com>
2015-08-23 07:59:20 +0100

    easyrpg-player: add 0.3 bottle.

--- easyrpg-player.rb
+++ easyrpg-player.rb
@@ -5,6 +5,13 @@ class EasyrpgPlayer < Formula
   sha256 "5c1201a36c555d80b8f30af758b74e60314f0f35fe084fe785b39ead7e2eff49"
   head "https://github.com/EasyRPG/Player.git"
 
+  bottle do
+    cellar :any
+    sha256 "f4b64852432ab4b694b51411a93708788c94b943bd2e61e74d0d0e4a5b9772af" => :yosemite
+    sha256 "eabee1db9cb20db5fc091c77388e73b4ae2b7c2ceee24aebe38bc24a73c471bc" => :mavericks
+    sha256 "a7e9b8c8df436ca437a521e905e7e13c0ace7595ddbdde430a72027795a0e2f1" => :mountain_lion
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
